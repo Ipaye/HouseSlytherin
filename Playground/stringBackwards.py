643ac0c5026a52a210713fadd24ab7c6d70af9fb
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-03-01 06:31:27 +0100

    :snake: learning persil tongue

---
+++ Playground/stringBackwards.py
@@ -0,0 +1,11 @@
+# fruit = 'mango'
+# length = len(fruit)
+# index = 0
+
+# for index in fruit:
+#     print(fruit[length-1])
+#     length -= 1
+
+days = ('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+print(days[2])
+print(type(days))
