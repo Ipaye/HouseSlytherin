f1ecd9dc7d52f82bb75ad601ef882ef55a39c991
Ipaye Alameen <Akinkunmi007@gmail.com>
2018-07-04 16:11:44 +0100

    Redid implementation of linkParser. [More effective]

--- Module4/linkParser2.py
+++ Module4/linkParser2.py
@@ -6,30 +6,40 @@ from bs4 import BeautifulSoup
 import ssl
 
 # Ignore SSL certificate errors
+# url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
+
+# Session handling
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-arr = []
+
 linkArr = []
 
-def getAllLinks(value):
-    i = 0
-    while i < 4:
-        html = urllib.request.urlopen(value, context=ctx).read()
-        handle = BeautifulSoup(html, 'html.parser')
+# All inputs
+url = input('Enter - ')
+position = int(input('Enter Position - '))
+position = position - 1
+count = int(input('Enter Count - '))
 
-        # Retrieve all of the anchor tags
-        aLinks = handle('a')
-        for lnk in aLinks:
-            print(lnk)
-            countLink = 1
-            if countLink == 3:
-                print(lnk.get('href', None))
-                linkArr.append(lnk.get('href', None))
-                arr.append(lnk.contents[0])
-            countLink += 1
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+# Retrieve all of the anchor tags
 
-        i += 1
+while count >= 1:
+    # Retrieve all of the anchor tags
+    links = soup('a')
+    print(links[position].get('href', None))
+    url = links[position].get('href', None)
 
-url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
-getAllLinks(url)
+    # Add names to array
+    linkArr.append(links[position].contents[0])
+
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    count = count - 1
+
+print(linkArr)
+print(linkArr[len(linkArr)-1])
+
+
+
